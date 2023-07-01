7143127853fb1b4832e4a7c151ee1e55c979cf0e
Pritom_Swarnakar <scsapn31@gmail.com>
2023-07-01 14:29:15 +0530

    My1

---
+++ Interf.java
@@ -0,0 +1,8 @@
+public class Interf {
+    public static void main(String[] args) {
+        Integer[] i= new Integer[10];
+        for(int m=0;m<i.length;m++){
+            System.out.println(i[m]);
+        }
+    }
+}
--- Sum.java
+++ Sum.java
@@ -2,8 +2,8 @@ public class Sum{
     public static void main(String[]args){
         int i=10;
         int j=20;
-        int sum=i+j;
-        System.out.println(sum);
+        int sum1=i+j;
+        System.out.println(sum1);
     }
 
 }
